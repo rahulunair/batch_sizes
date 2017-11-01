3b00cf0a2f5c1b957aef210151622bb59f935940
Rahul Nair <rahulunair@gmail.com>
2017-11-01 11:42:13 -0700

    Added initial version, should add the test results as well

---
+++ main.py
@@ -0,0 +1,95 @@
+"""Effect of different batchsizes on test loss"""
+#!/usr/bin/env python
+
+from collections import OrderedDict
+
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+from tqdm import tqdm
+
+
+# model params
+inp, hid, out = 784, 50, 10
+batch_size = [8, 16, 32, 64, 128, 512, 1024]
+epochs = 10
+lr = 0.01
+
+
+class NN(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.l1 = nn.Linear(inp, hid)
+        self.h1 = nn.Linear(hid, hid)
+        self.l2 = nn.Linear(hid, out)
+
+    def forward(self, x):
+        x = x.view((-1, 784))
+        x = F.relu(self.l1(x))
+        x = F.relu(self.h1(x))
+        return F.softmax(self.l2(x))
+
+nn = NN()
+optim = torch.optim.SGD(nn.parameters(), lr=lr)
+
+
+def train(epoch):
+    nn.train()
+    print("Training Epoch : {}".format(epoch))
+    for data, label in tqdm(train_loader):
+        data, label = Variable(data), Variable(label)
+        optim.zero_grad()
+        output = nn(data)
+        loss = F.cross_entropy(output, label)
+        loss.backward()
+        optim.step()
+    print("Training Loss {:-4f}".format(loss.data[0]))
+    return loss.data[0]
+
+
+def test():
+    nn.eval()
+    print("Testing..")
+    test_loss = 0
+    for data, label in tqdm(test_loader):
+        data, label = Variable(data), Variable(label)
+        output = nn(data)
+        test_loss += F.cross_entropy(output, label, size_average=False).data[0]
+        pred = output.data.max(1, keepdim=True)[1]
+    test_loss /= len(test_loader.dataset)
+    # print("Test Epoch {}, Average Loss {:-4f}".format(epoch, test_loss))
+    return test_loss
+
+
+if __name__ == "__main__":
+    batch_loss = OrderedDict()
+    # load and preprocess MNIST dataset
+    transforms = transforms.Compose([transforms.ToTensor(),
+                                     transforms.Normalize((0.1307, ),
+                                                          (0.3081, ))])
+    for b_size in batch_size:
+        train_loader = torch.utils.data.DataLoader(
+            datasets.MNIST("/tmp", train=True, download=True,
+                           transform=transforms),
+            batch_size=b_size)
+        test_loader = torch.utils.data.DataLoader(
+            datasets.MNIST("/tmp", train=False, download=True,
+                           transform=transforms),
+            batch_size=b_size)
+        loss = []
+        for e in range(1, epochs + 1):
+            loss.append(train(e))  # comment this line if the line below is uncommented
+            # loss.append(test())  # testing loss if you are into that kind of things
+        batch_loss[b_size] = sum(loss)/len(loss)
+    for k, v in batch_loss.items():
+        print("{} => {:-4f}".format(k, v))
+    items = batch_loss.items()
+    x, y = zip(*items)
+    plt.plot(x, y)
+    plt.ylabel("Training loss")
+    plt.xlabel("Mini batch sizes")
+    plt.show()
